95de391d4b3849828a1f4abfd411c7e7ae7dd4f8
Manuel <huilcashuancaa@gmail.com>
2023-02-10 22:06:36 -0500

    Comparacion-PHP

--- IntroduccionPHP_inicio/05-comparacion.php
+++ IntroduccionPHP_inicio/05-comparacion.php
@@ -1,7 +1,45 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 30;
+$numero3 = 30;
+$numero4 = "30";
 
+var_dump($numero1 > $numero2);
+echo "<br>";
 
+var_dump($numero1 < $numero2);
+echo "<br>";
 
+var_dump($numero1 >= $numero2);
+echo "<br>";
+
+var_dump($numero1 <= $numero2);
+echo "<br>";
+
+var_dump($numero2 == $numero3);
+echo "<br>";
+
+var_dump($numero2 == $numero4);
+echo "<br>";
+// compara el tipo de dato y su valor
+var_dump($numero2 === $numero4);
+echo "<br>";
+
+// spaceship
+//-1 si izquierdaes menor,0 ssi es igual ,1 si izquierda es mayor
+
+// Si el primer valor es mayor que el segundo, el resultado será 1.
+// Si el primer valor es igual al segundo, el resultado será 0.
+// Si el primer valor es menor que el segundo, el resultado será -1.
+
+var_dump($numero1 <=> $numero2);
+echo "<br>";
+
+var_dump($numero2 <=> $numero3);
+echo "<br>";
+
+var_dump($numero2 <=> $numero1);
+echo "<br>";
 
 include 'includes/footer.php';
