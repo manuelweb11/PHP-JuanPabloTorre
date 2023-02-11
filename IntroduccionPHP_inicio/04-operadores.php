42a8cd2ca2835ff64c36bd664316ba2e0c0873f8
Manuel <huilcashuancaa@gmail.com>
2023-02-10 21:34:42 -0500

    Operadores

--- IntroduccionPHP_inicio/04-operadores.php
+++ IntroduccionPHP_inicio/04-operadores.php
@@ -1,5 +1,26 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+
+// Sumar
+echo $numero1 + $numero2;
+echo "<br>";
+
+// resta
+echo $numero1 - $numero2;
+echo "<br>";
+
+// multiplicar
+echo $numero1* $numero2;
+echo "<br>";
+
+//dividir
+echo $numero1/$numero2;
+echo "<br>";
+
+// Multiplicar cierta cantidad
+echo 2 ** 3;
 
 
 
